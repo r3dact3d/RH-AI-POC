ff41e3bc166c66db1b7a7a9f32754e0da548d0d1
r3dact3d <redhatbrady@gmail.com>
2025-03-19 10:40:53 -0500

    test trigger

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ resource "tls_private_key" "cloud_key" {
 # Add key for ssh connection
 resource "aws_key_pair" "cloud_key" {
   key_name   = "cloud_key"
-  public_key = tls_private_key.cloud_key.public_key_openssh
+  public_key = tls_private_key.rhelai_cloud_key.public_key_openssh
 }
 
 resource "aws_vpc" "rhelai_vpc" {
@@ -207,7 +207,7 @@ resource "aws_instance" "rhelai_instance" {
   instance_type               = "g4dn.xlarge"
   vpc_security_group_ids      = [aws_security_group.rhelai_security_group.id]
   associate_public_ip_address = true
-  key_name        = aws_key_pair.cloud_key.key_name
+  key_name                    = aws_key_pair.rhelai_cloud_key.key_name
   user_data                   = file("user_data.txt")
   #ami                         = data.aws_ami.rhel.id
   ami                         = "ami-00dfd618096881315"
@@ -241,6 +241,10 @@ resource "null_resource" "hostname_update" {
 
       # Set hostname
       "sudo hostnamectl set-hostname ${aws_instance.rhelai_instance.public_dns}",
+
+      # Setup Cloud SSH Keys
+      "echo ${tls_private_key.rhelai_cloud_key.private_key_pem} >> /home/ec2-user/.ssh/cloud_keys",
+      "chmod 0644 /home/ec2-user/.ssh/cloud_keys",
 
 
 # Add created ec2 instance to ansible inventory
